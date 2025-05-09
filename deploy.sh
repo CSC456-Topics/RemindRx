ca126e6fc70a0c67f2a8218c6dec6e0ff0ae6b5e
Abrar Habib <abrarhabib285@gmail.com>
2025-05-09 17:24:37 -0400

    fix: deployment successful

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # $1 is the version number passed as an argument
-ZIP_FILE="myapp_deploy-$1.zip"
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+ZIP_FILE="myapp_deploy-$1-$TIMESTAMP.zip"
 
 # Create a zip with the required folders and files
 zip -r "$ZIP_FILE" \
@@ -10,7 +11,7 @@ zip -r "$ZIP_FILE" \
   ./recommend \
   ./requirements.txt \
   ./.ebextensions \
-  ./Procfile \
+  ./Procfile
 
 # Upload the artifact to S3
 aws s3 cp "$ZIP_FILE" s3://elasticbeanstalk-us-east-1-433574703961
@@ -19,12 +20,12 @@ aws s3 cp "$ZIP_FILE" s3://elasticbeanstalk-us-east-1-433574703961
 aws elasticbeanstalk create-application-version \
   --application-name flaskbb \
   --source-bundle S3Bucket="elasticbeanstalk-us-east-1-433574703961",S3Key="$ZIP_FILE" \
-  --version-label "ver-$1" \
+  --version-label "ver-$1-$TIMESTAMP" \
   --description "New deploy version" \
   --region "us-east-1"
 
 # Update the Elastic Beanstalk environment
 aws elasticbeanstalk update-environment \
   --environment-name flaskbb-env \
-  --version-label "ver-$1" \
+  --version-label "ver-$1-$TIMESTAMP" \
   --region "us-east-1"
