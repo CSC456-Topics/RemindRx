85357808f2e43aaac2119f02ed8cb2426d4ffacd
Ivan Chen <ivanchen498@gmail.com>
2025-05-10 17:12:05 -0400

    downgraded node version, may be root cause of 502 bad gateway on production application on beanstalk

--- frontend_terraform/elastic_beanstalk.tf
+++ frontend_terraform/elastic_beanstalk.tf
@@ -6,7 +6,7 @@ resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "nextapp-environment"
   cname_prefix        = "nextappIvanC112"
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2023 v6.5.1 running Node.js 22"
+  solution_stack_name = "64bit Amazon Linux 2023 v6.5.1 running Node.js 18"
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
