5dfd70cdc27d18c80d6907168be7f3cab067bee0
Ivan Chen <ivanchen498@gmail.com>
2025-05-10 14:21:39 -0400

    update playwright job failing initially with port 3000

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://127.0.0.1:8080',
     reuseExistingServer: !process.env.CI,
   },
 });
